b0b854db17937e48b181457f4044c6d892f9b3ae
vanessaborges2 <vanessa.borges2@fatec.sp.gov.br>
2025-03-11 22:26:20 -0300

    Projeto Banco

---
+++ projetobanco/database/seeders/CategoriaSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categoria;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Categoria::create(["nome" => 'Categoria1']);
+        Categoria::create(['nome' => 'Categoria2']);
+    }
+}
